09e24f6a873a4307362d2ddc28b21022e2a3213a
ThomyThom <thomaz.camargo30@gmail.com>
2026-08-28 11:04:42 -0300

    add soma e divisão

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function subtrair() {
     let resultado = numero1 - numero2;
     document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
 }
+
+function multiplicar() {
+    const num1 = Number(document.getElementById('numero1').value);
+    const num2 = Number(document.getElementById('numero2').value);
+    document.getElementById('resultado').innerText = `Resultado: ${num1 * num2}`;
+}
+
+function dividir() {
+    const num1 = Number(document.getElementById('numero1').value);
+    const num2 = Number(document.getElementById('numero2').value);
+    
+    if (num2 === 0) {
+        document.getElementById('resultado').innerText = "Erro: Divisão por zero!";
+        return;
+    }
+    
+    document.getElementById('resultado').innerText = `Resultado: ${num1 / num2}`;
+}
